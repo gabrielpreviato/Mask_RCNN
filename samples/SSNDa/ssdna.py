a0598c7083b356e6b74433154b0325274788ea7a
Gabriel Previato <gabriel.previato@gmail.com>
2019-09-16 17:53:00 -0300

    [WIP] Updated SSDNa dataset class

--- samples/SSNDa/ssdna.py
+++ samples/SSNDa/ssdna.py
@@ -31,17 +31,7 @@ import os
 import sys
 import time
 import numpy as np
-import imgaug  # https://github.com/aleju/imgaug (pip3 install imgaug)
-
-# Download and install the Python COCO tools from https://github.com/waleedka/coco
-# That's a fork from the original https://github.com/pdollar/coco with a bug
-# fix for Python 3.
-# I submitted a pull request https://github.com/cocodataset/cocoapi/pull/50
-# If the PR is merged then use the original repo.
-# Note: Edit PythonAPI/Makefile and replace "python" with "python3".
-from pycocotools.coco import COCO
-from pycocotools.cocoeval import COCOeval
-from pycocotools import mask as maskUtils
+import glob
 
 import zipfile
 import urllib.request
@@ -86,64 +76,149 @@ class SsndaConfig(Config):
     # Number of classes (including background)
     NUM_CLASSES = 1 + 4  # SSNDa has 4 classes
 
+    WIDTH = 260
+    HEIGHT = 160
+    NUMBER_OBSTACLES = = 13
+
 
 ############################################################
 #  Dataset
 ############################################################
 
 class SsndaDataset(utils.Dataset):
-    def load_coco(self, dataset_dir, subset, year=DEFAULT_DATASET_YEAR, class_ids=None,
-                  class_map=None, return_coco=False, auto_download=False):
-        """Load a subset of the COCO dataset.
-        dataset_dir: The root directory of the COCO dataset.
+    def load_coco(self, dataset_dir, subset, class_ids=None,
+                  class_map=None, return_ssnda=False, auto_download=False):
+        """Load a subset of the SSNDa dataset.
+        dataset_dir: The root directory of the SSNDa dataset.
         subset: What to load (train, val, minival, valminusminival)
-        year: What dataset year to load (2014, 2017) as a string, not an integer
         class_ids: If provided, only loads images that have the given classes.
         class_map: TODO: Not implemented yet. Supports maping classes from
             different datasets to the same class ID.
-        return_coco: If True, returns the COCO object.
-        auto_download: Automatically download and unzip MS-COCO images and annotations
+        return_ssnda: If True, returns the SSNDa object.
+        auto_download: Automatically download and unzip SSNDa images and annotations
         """
 
-        if auto_download is True:
-            self.auto_download(dataset_dir, subset, year)
+        # if auto_download is True:
+        #     self.auto_download(dataset_dir, subset)
 
-        coco = COCO("{}/annotations/instances_{}{}.json".format(dataset_dir, subset, year))
-        if subset == "minival" or subset == "valminusminival":
-            subset = "val"
-        image_dir = "{}/{}{}".format(dataset_dir, subset, year)
+        # ssnda = SSNDa("{}/annotations/instances_{}{}.json".format(dataset_dir, subset, year))
+        # if subset == "minival" or subset == "valminusminival":
+        #     subset = "val"
+        # image_dir = "{}/{}{}".format(dataset_dir, subset, year)
 
-        # Load all classes or a subset?
-        if not class_ids:
-            # All classes
-            class_ids = sorted(coco.getCatIds())
+        # # Load all classes or a subset?
+        # if not class_ids:
+        #     # All classes
+        #     class_ids = sorted(coco.getCatIds())
 
-        # All images or a subset?
-        if class_ids:
-            image_ids = []
-            for id in class_ids:
-                image_ids.extend(list(coco.getImgIds(catIds=[id])))
-            # Remove duplicates
-            image_ids = list(set(image_ids))
-        else:
-            # All images
-            image_ids = list(coco.imgs.keys())
+        # # All images or a subset?
+        # if class_ids:
+        #     image_ids = []
+        #     for id in class_ids:
+        #         image_ids.extend(list(coco.getImgIds(catIds=[id])))
+        #     # Remove duplicates
+        #     image_ids = list(set(image_ids))
+        # else:
+        #     # All images
+        #     image_ids = list(coco.imgs.keys())
 
-        # Add classes
-        for i in class_ids:
-            self.add_class("coco", i, coco.loadCats(i)[0]["name"])
+        # # Add classes
+        # for i in class_ids:
+        #     self.add_class("coco", i, coco.loadCats(i)[0]["name"])
+
+        # Add classes.
+        self.add_class("ssdna", 1, "goal")
+        self.add_class("ssdna", 2, "ball")
+        self.add_class("ssdna", 3, "robot_team")
+        self.add_class("ssdna", 4, "robot_opponent")
+
+        # Train or validation dataset?
+        assert subset in ["train", "val"]
+        dataset_dir = os.path.join(dataset_dir, subset)
 
         # Add images
-        for i in image_ids:
+        images_path = sorted(glob.glob(os.path.join(dataset_dir, subset, 'rgb', '*' + '.png')))
+        for img_path in images_path:
             self.add_image(
-                "coco", image_id=i,
-                path=os.path.join(image_dir, coco.imgs[i]['file_name']),
-                width=coco.imgs[i]["width"],
-                height=coco.imgs[i]["height"],
-                annotations=coco.loadAnns(coco.getAnnIds(
-                    imgIds=[i], catIds=class_ids, iscrowd=None)))
-        if return_coco:
-            return coco
+                "ssdna",
+                image_id=img_path,
+                path=img_path,
+                width=SsndaConfig.WIDTH,
+                height=SsndaConfig.HEIGHT)
+        # if return_ssnda:
+        #     return ssnda
+
+    # def auto_download(self, dataDir, dataType):
+    #     """Download the COCO dataset/annotations if requested.
+    #     dataDir: The root directory of the COCO dataset.
+    #     dataType: What to load (train, val, minival, valminusminival)
+    #     dataYear: What dataset year to load (2014, 2017) as a string, not an integer
+    #     Note:
+    #         For 2014, use "train", "val", "minival", or "valminusminival"
+    #         For 2017, only "train" and "val" annotations are available
+    #     """
+
+    #     # Setup paths and file names
+    #     if dataType == "minival" or dataType == "valminusminival":
+    #         imgDir = "{}/{}{}".format(dataDir, "val", dataYear)
+    #         imgZipFile = "{}/{}{}.zip".format(dataDir, "val", dataYear)
+    #         imgURL = "http://images.cocodataset.org/zips/{}{}.zip".format("val", dataYear)
+    #     else:
+    #         imgDir = "{}/{}{}".format(dataDir, dataType, dataYear)
+    #         imgZipFile = "{}/{}{}.zip".format(dataDir, dataType, dataYear)
+    #         imgURL = "http://images.cocodataset.org/zips/{}{}.zip".format(dataType, dataYear)
+    #     # print("Image paths:"); print(imgDir); print(imgZipFile); print(imgURL)
+
+    #     # Create main folder if it doesn't exist yet
+    #     if not os.path.exists(dataDir):
+    #         os.makedirs(dataDir)
+
+    #     # Download images if not available locally
+    #     if not os.path.exists(imgDir):
+    #         os.makedirs(imgDir)
+    #         print("Downloading images to " + imgZipFile + " ...")
+    #         with urllib.request.urlopen(imgURL) as resp, open(imgZipFile, 'wb') as out:
+    #             shutil.copyfileobj(resp, out)
+    #         print("... done downloading.")
+    #         print("Unzipping " + imgZipFile)
+    #         with zipfile.ZipFile(imgZipFile, "r") as zip_ref:
+    #             zip_ref.extractall(dataDir)
+    #         print("... done unzipping")
+    #     print("Will use images in " + imgDir)
+
+    #     # Setup annotations data paths
+    #     annDir = "{}/annotations".format(dataDir)
+    #     if dataType == "minival":
+    #         annZipFile = "{}/instances_minival2014.json.zip".format(dataDir)
+    #         annFile = "{}/instances_minival2014.json".format(annDir)
+    #         annURL = "https://dl.dropboxusercontent.com/s/o43o90bna78omob/instances_minival2014.json.zip?dl=0"
+    #         unZipDir = annDir
+    #     elif dataType == "valminusminival":
+    #         annZipFile = "{}/instances_valminusminival2014.json.zip".format(dataDir)
+    #         annFile = "{}/instances_valminusminival2014.json".format(annDir)
+    #         annURL = "https://dl.dropboxusercontent.com/s/s3tw5zcg7395368/instances_valminusminival2014.json.zip?dl=0"
+    #         unZipDir = annDir
+    #     else:
+    #         annZipFile = "{}/annotations_trainval{}.zip".format(dataDir, dataYear)
+    #         annFile = "{}/instances_{}{}.json".format(annDir, dataType, dataYear)
+    #         annURL = "http://images.cocodataset.org/annotations/annotations_trainval{}.zip".format(dataYear)
+    #         unZipDir = dataDir
+    #     # print("Annotations paths:"); print(annDir); print(annFile); print(annZipFile); print(annURL)
+
+    #     # Download annotations if not available locally
+    #     if not os.path.exists(annDir):
+    #         os.makedirs(annDir)
+    #     if not os.path.exists(annFile):
+    #         if not os.path.exists(annZipFile):
+    #             print("Downloading zipped annotations to " + annZipFile + " ...")
+    #             with urllib.request.urlopen(annURL) as resp, open(annZipFile, 'wb') as out:
+    #                 shutil.copyfileobj(resp, out)
+    #             print("... done downloading.")
+    #         print("Unzipping " + annZipFile)
+    #         with zipfile.ZipFile(annZipFile, "r") as zip_ref:
+    #             zip_ref.extractall(unZipDir)
+    #         print("... done unzipping")
+    #     print("Will use annotations in " + annFile)
 
     def load_mask(self, image_id):
         """Load instance masks for the given image.
@@ -159,43 +234,14 @@ class SsndaDataset(utils.Dataset):
         """
         # If not a COCO image, delegate to parent class.
         image_info = self.image_info[image_id]
-        if image_info["source"] != "coco":
-            return super(CocoDataset, self).load_mask(image_id)
+        if image_info["source"] != "ssnda":
+            return super(SsndaDataset, self).load_mask(image_id)
 
-        instance_masks = []
-        class_ids = []
-        annotations = self.image_info[image_id]["annotations"]
-        # Build mask of shape [height, width, instance_count] and list
-        # of class IDs that correspond to each channel of the mask.
-        for annotation in annotations:
-            class_id = self.map_source_class_id(
-                "coco.{}".format(annotation['category_id']))
-            if class_id:
-                m = self.annToMask(annotation, image_info["height"],
-                                   image_info["width"])
-                # Some objects are so small that they're less than 1 pixel area
-                # and end up rounded out. Skip those objects.
-                if m.max() < 1:
-                    continue
-                # Is it a crowd? If so, use a negative class ID.
-                if annotation['iscrowd']:
-                    # Use negative class ID for crowds
-                    class_id *= -1
-                    # For crowd masks, annToMask() sometimes returns a mask
-                    # smaller than the given dimensions. If so, resize it.
-                    if m.shape[0] != image_info["height"] or m.shape[1] != image_info["width"]:
-                        m = np.ones([image_info["height"], image_info["width"]], dtype=bool)
-                instance_masks.append(m)
-                class_ids.append(class_id)
-
-        # Pack instance masks into an array
-        if class_ids:
-            mask = np.stack(instance_masks, axis=2).astype(np.bool)
-            class_ids = np.array(class_ids, dtype=np.int32)
-            return mask, class_ids
-        else:
-            # Call super class to return an empty mask
-            return super(CocoDataset, self).load_mask(image_id)
+         # Convert polygons to a bitmap mask of shape
+        # [height, width, instance_count]
+        info = self.image_info[image_id]
+        mask = np.zeros([info["height"], info["width"], SsndaConfig.NUMBER_OBSTACLES],
+                        dtype=np.uint8)
 
     def image_reference(self, image_id):
         """Return a link to the image in the COCO Website."""
